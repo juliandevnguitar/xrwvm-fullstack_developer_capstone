33a6e36d679201a02d8430ca9d8227fa42347c2d
Julian García <138516573+juliandevnguitar@users.noreply.github.com>
2024-08-10 21:24:50 -0600

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -22,7 +22,7 @@ def initiate():
          "description": "Great cars. Japanese technology",
          "year_foundation": 1975,
          "ceo": "Bob Smith"},]
-    
+
     car_make_instances = []
     for data in car_make_data:
         car_make_instances.append(
@@ -49,72 +49,72 @@ def initiate():
             "year": 2023,
             "car_make": car_make_instances[0],
             "color": "RED"},
-      {
+        {
           "name": "A-Class",
-          "type": "SUV", "year" : 2023,
+          "type": "SUV", "year": 2023,
           "car_make": car_make_instances[1],
           "color": "BLACK"},
-      {
+        {
           "name": "C-Class",
           "type": "SUV",
           "year": 2023,
           "car_make": car_make_instances[1],
           "color": "WHITE"},
-      {
+        {
           "name": "E-Class",
           "type": "SUV",
           "year": 2023,
           "car_make": car_make_instances[1],
           "color": "GREEN"},
-      {
+        {
           "name": "A4",
           "type": "SUV",
           "year": 2023,
           "car_make": car_make_instances[2],
           "color": "BLACK"},
-      {
+        {
           "name": "A5",
           "type": "SUV",
           "year": 2023,
           "car_make": car_make_instances[2],
           "color": "WHITE"},
-      {
+        {
           "name": "A6",
           "type": "SUV",
           "year": 2023,
           "car_make": car_make_instances[2],
           "color": "BLACK"},
-      {
+        {    
           "name": "Sorrento",
           "type": "SUV",
           "year": 2023,
           "car_make": car_make_instances[3],
           "color": "BLACK"},
-      {
+        {
           "name": "Carnival",
           "type": "SUV",
           "year": 2023,
           "car_make": car_make_instances[3],
           "color": "WHITE"},
-      {
+        {
           "name": "Cerato",
           "type": "Sedan",
           "year": 2023,
           "car_make": car_make_instances[3],
           "color": "BLACK"},
-      {
+        {
           "name": "Corolla",
           "type": "Sedan",
           "year": 2023,
           "car_make": car_make_instances[4],
           "color": "BLUE"},
-      {
+        {
           "name": "Camry",
           "type": "Sedan",
           "year": 2023,
           "car_make": car_make_instances[4],
           "color": "BLACK"},
-      {
+        {
           "name": "Kluger",
           "type": "SUV",
           "year": 2023,
@@ -123,7 +123,7 @@ def initiate():
         # Add more CarModel instances as needed
   ]
     for data in car_model_data:
-      CarModel.objects.create(name=data['name'],
-                              car_make=data['car_make'],
-                              type=data['type'],
-                              year=data['year'])
+        CarModel.objects.create(name=data['name'],
+                                car_make=data['car_make'],
+                                type=data['type'],
+                                year=data['year'])
